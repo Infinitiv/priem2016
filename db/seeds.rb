eeabec903b95e785d0250ae190ee840d443c4094
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2019-07-21 13:43:18 +0300

    add 53 target organization

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,1 @@
-TargetOrganization.create(id: 51, target_organization_name: 'ГБУЗ "Самарская ОКБ им. Середавина"')
-TargetOrganization.create(id: 52, target_organization_name: 'ОБУЗ ГКБ №7')
+TargetOrganization.create(id: 53, target_organization_name: 'ФБУЗ "Приволжский окружной медицинский центр"')
