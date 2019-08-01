e130d11fb0d29bc4710215af0c5bbb653fd0b1a3
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2019-08-01 08:09:16 +0300

    fix mon report again

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,7 +4,7 @@ class ReportsController < ApplicationController
   def mon
     @entrance_test_items = @campaign.entrance_test_items.order(:entrance_test_priority).select(:subject_id, :min_score, :entrance_test_priority).uniq
     @admission_volume_hash = EntrantApplication.admission_volume_hash(@campaign)
-    @applications_hash = EntrantApplication.entrant_applications_hash(@campaign).select{|k, v| v[:summa] > 0 && k.status_id == 4}.select{|k, v| v[:summa] > 0}.sort_by{|k, v| [v[:full_summa].to_f, v[:summa].to_f, v[:mark_values], v[:benefit]]}.reverse
+    @applications_hash = EntrantApplication.entrant_applications_hash(@campaign)
     @target_organizations = TargetOrganization.order(:target_organization_name)
   end
     
