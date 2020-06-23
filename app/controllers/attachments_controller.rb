0016b890a7d063a5698ffb839d41a2abfb44da01
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2020-06-23 06:13:13 +0000

    fix attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,19 +3,18 @@ class AttachmentsController < ApplicationController
   before_action :set_attachment, only: [:show, :destroy]
   
   def show
+    path = @attachment.data_hash[0..2].split('').join('/')
+    send_file Rails.root.join('storage', path, @attachment.data_hash), :filename => @attachment.filename, :type => @attachment.mime_type
   end
     
   def destroy
-    if @attachment.destroy
-      path = @attachment.data_hash[0..2].split('').join('/')
-      File.delete(Rails.root.join('storage', path, file_name)) if file_name
-    end
+    @attachment.destroy
     redirect_to :back
   end
   
   private
   
   def set_attachment
-    @attachment = Attachment.find_by_data_hash(params[:id])
+    @attachment = Attachment.find(params[:id])
   end
 end
