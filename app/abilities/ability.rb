66b9f5aa069b550ccc8be2d46789b60929f3f9ed
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2020-07-08 10:03:11 +0000

    add ability add document

--- app/abilities/ability.rb
+++ app/abilities/ability.rb
@@ -5,7 +5,7 @@ class Ability
     return unless user.present?
     can :read, Campaign
     return unless user.clerk?
-    can [:read, :toggle_agreement, :toggle_original, :entrant_application_recall, :toggle_contract, :destroy, :approve, :generate_templates, :add_comment, :delete_comment, :update, :delete_request], EntrantApplication
+    can [:read, :toggle_agreement, :toggle_original, :entrant_application_recall, :toggle_contract, :destroy, :approve, :generate_templates, :add_comment, :delete_comment, :update, :delete_request, :add_document], EntrantApplication
     can [:update], IdentityDocument
     can [:update], EducationDocument
     can [:update], TargetContract
@@ -16,9 +16,8 @@ class Ability
     return unless user.super?
     can [:manage], Achievement
     can [:manage], TargetContract
-    can [:toggle_competitive_group], EntrantApplication
     can [:mon], Report
-    can [:competition_lists, :competition_lists_to_html], EntrantApplication
+    can [:competition_lists, :competition_lists_to_html, :toggle_competitive_group], EntrantApplication
     can [:destroy], Attachment
     return unless user.admin?
     can :manage, :all
