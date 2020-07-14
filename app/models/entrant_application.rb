e10e72da5582717cf0784860bb1bbc798988f908
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2020-07-14 11:00:57 +0000

    add journals to entrant application model

--- app/models/entrant_application.rb
+++ app/models/entrant_application.rb
@@ -16,6 +16,7 @@ class EntrantApplication < ActiveRecord::Base
   has_many :olympic_documents, dependent: :destroy
   has_many :other_documents, dependent: :destroy
   has_many :attachments, dependent: :destroy
+  has_many :journals, dependent: :destroy
   
 #   validates :application_number, :campaign_id, :entrant_last_name, :entrant_first_name, :gender_id, :birth_date, :registration_date, :status_id, :data_hash, presence: true
   
