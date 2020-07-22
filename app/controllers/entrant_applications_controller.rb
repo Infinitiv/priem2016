51be488562cd44aecd9dfe9c41c0511874b3832d
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2020-07-22 10:13:10 +0000

    fix ord export applications collection

--- app/controllers/entrant_applications_controller.rb
+++ app/controllers/entrant_applications_controller.rb
@@ -241,7 +241,7 @@ class EntrantApplicationsController < ApplicationController
   end
   
   def ord_export
-    @entrant_applications = @campaign.entrant_applications.select(:id, :snils, :entrant_last_name, :entrant_first_name, :entrant_middle_name, :birth_date, :nationality_type_id, :registration_date, :return_documents_date).includes(:marks, :competitive_groups, :education_document).where(return_documents_date: nil, nationality_type_id: 1)
+    @entrant_applications = @campaign.entrant_applications.select(:id, :snils, :entrant_last_name, :entrant_first_name, :entrant_middle_name, :birth_date, :nationality_type_id, :registration_date, :return_documents_date).includes(:marks, :competitive_groups, :education_document).where(return_documents_date: nil, nationality_type_id: 1, status_id: [4, 6])
     send_data EntrantApplication.ord_export(@entrant_applications), filename: "entrant_applications-#{Date.today}.csv", type: 'text/csv', disposition: "attachment"
   end
   
