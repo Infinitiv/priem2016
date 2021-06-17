12cde3287128aa88b1a39d6277677d69b16ddc7c
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2021-06-17 19:07:49 +0000

    update cap lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.14.1'
+lock '3.16.0'
 
 set :application, 'priem2016_production'
 set :repo_url, 'git@github.com:Infinitiv/priem2016.git'
