588e5634f1024c18fa16dd506afb97c3a63981cc
Vladimir Markovnin <markovnin@isma.ivanovo.ru>
2021-08-06 15:32:59 +0000

    ord competition list

--- app/controllers/entrant_applications_controller.rb
+++ app/controllers/entrant_applications_controller.rb
@@ -251,7 +251,7 @@ class EntrantApplicationsController < ApplicationController
   def competition_lists_ord_to_html
     @entrance_test_items = @campaign.entrance_test_items.order(:entrance_test_priority).select(:subject_id, :min_score, :entrance_test_priority).uniq
     @admission_volume_hash = EntrantApplication.admission_volume_hash(@campaign)
-    @applications_hash = EntrantApplication.entrant_applications_hash(@campaign).select{|k, v| v[:summa] > 0 && k.status_id == 4}.select{|k, v| v[:summa] > 0}.sort_by{|k, v| [v[:full_summa].to_f, v[:summa].to_f, v[:mark_values], v[:benefit]]}.reverse
+    @applications_hash = EntrantApplication.entrant_applications_hash(@campaign).select{|k, v| v[:summa] > 0 && k.status_id == 4}.select{|k, v| v[:summa] > 0}.sort_by{|k, v| [v[:full_summa].to_f, v[:summa].to_f]}.reverse
     @target_organizations = TargetOrganization.order(:target_organization_name)
     html = render_to_string layout: 'competition_lists_to_html'
     filename = "#{@campaign.id}-#{Time.now.to_datetime.strftime("%F %T")}.html".gsub(' ', '-')
